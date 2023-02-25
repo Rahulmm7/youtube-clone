66471fa0cefc4412ae217986d70fd41cdd1ca8ee
Rahul <rahulmanohar007@gmail.com>
2023-02-25 09:45:28 +0530

    video section

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,25 @@
+import { Box, Stack } from '@mui/material'
 import React from 'react'
+import { VideoCard, ChannelCard } from './'
 
 const Videos = ({ videos }) => {
 
     return (
-        < div > Videos</div >
+        <Stack direction="row"
+            flexWrap="wrap"
+            justifyContent="flex-start"
+            gap={2}
+        >{
+                videos.map((item, idx) => (
+                    <Box key={idx}>
+                        {item.id.videoId && <VideoCard video={item} />}
+                        {item.id.channelId && <ChannelCard channelDetail={item} />}
+
+                    </Box>
+
+                ))}
+
+        </Stack>
     )
 }
 
